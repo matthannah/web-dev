8b87dfab0ffba5f236fbb8bfda919f65229e045f
matthannah <mryanhannah@gmail.com>
2017-03-23 19:44:24 +1100

    NEW: Add basic react usage

---
+++ react-components/src/scripts/header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// export a react component named Footer
+export default class Header extends React.Component {
+  // remember a react component must implement the render() method.
+  render() {
+    // render a h1
+    return (
+      <h1>
+        this is the header
+      </h1>
+    );
+  }
+}
